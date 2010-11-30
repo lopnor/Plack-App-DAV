01676d110adf6806a63836a343f5bc625397bea2
Nobuo Danjou <nobuo.danjou@gmail.com>
2010-11-30 14:41:31 +0900

    initial commit

---
+++ lib/Plack/App/DAV.pm
@@ -0,0 +1,32 @@
+package Plack::App::DAV;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::App::DAV -
+
+=head1 SYNOPSIS
+
+  use Plack::App::DAV;
+
+=head1 DESCRIPTION
+
+Plack::App::DAV is
+
+=head1 AUTHOR
+
+Nobuo Danjou E<lt>nobuo.danjou@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
